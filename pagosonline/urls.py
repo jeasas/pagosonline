eef023401da20e220604976e70f8be472e589dbd
Ariel Núñez <ingenieroariel@gmail.com>
2012-04-21 12:06:21 -0400

    Fixed urls to point to simplified namespace

--- pagosonline/urls.py
+++ pagosonline/urls.py
@@ -11,11 +11,9 @@ from satchmo_store.shop.satchmo_settings import get_satchmo_setting
 ssl = get_satchmo_setting('SSL', default_value=False)
 
 urlpatterns = patterns('',
-    (r'^$', 'payment.modules.pagosonline.views.pay_ship_info', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-step2'),
-    (r'^confirm/$', 'payment.modules.pagosonline.views.confirm_info', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-step3'),
-#    (r'^success/$', 'payment.views.checkout.success', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-success'),
-#    (r'^failure/$', 'payment.views.checkout.failure', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-failure'),
-    (r'^answer/$', 'payment.modules.pagosonline.views.answerpay', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-answer'),
-    (r'^notify/$', 'payment.modules.pagosonline.views.notify_callback', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-notify_callback'),
+    (r'^$', 'pagosonline.views.pay_ship_info', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-step2'),
+    (r'^confirm/$', 'pagosonline.views.confirm_info', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-step3'),
+    (r'^answer/$', 'pagosonline.views.answerpay', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-answer'),
+    (r'^notify/$', 'pagosonline.views.notify_callback', {'SSL': ssl}, 'PAGOSONLINE_satchmo_checkout-notify_callback'),
     (r'^confirmorder/$', 'payment.views.confirm.confirm_free_order', {'SSL' : ssl, 'key' : 'PAGOSONLINE'}, 'PAGOSONLINE_satchmo_checkout_free-confirm')
 )
