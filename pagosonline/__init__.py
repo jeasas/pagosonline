dd7f5042358118c8d5eec7808aecb6523f6cdf4d
Ariel Núñez <ingenieroariel@gmail.com>
2012-04-21 12:00:37 -0400

    Added __version__ to __init__.py

---
+++ pagosonline/__init__.py
@@ -0,0 +1,7 @@
+# Make the version available if there is a version.py file
+try:
+    from extract.version import version as __version__
+    from extract.version import git_revision as __git_revision__
+except ImportError:
+    __version__ = "unknwon"
+    __git_revision__ = None
